0777c7e67a9345bfc9da1f0c3896bba28f0f6601
Artem Trofymenko <randallsng@gmail.com>
2023-01-27 19:58:04 +0100

    commit msg

---
+++ Lesson14(Db config from file prop)/src/main/java/org/example/dao/PersonDAO.java
@@ -0,0 +1,42 @@
+package org.example.dao;
+
+import org.example.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    //    private static int PEOPLE_COUNT;
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> index() {
+        return jdbcTemplate.query("Select * from person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person show(int id) {
+      return jdbcTemplate.query("Select * from person where id =?",new Object[]{id},new BeanPropertyRowMapper<>(Person.class))
+              .stream().findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+       jdbcTemplate.update("Insert into person values (1,?,?,?)",
+                person.getName(),person.getAge(),person.getEmail());
+    }
+
+    public void update(int id, Person person) {
+       jdbcTemplate.update("Update person set name=?,age=?,email=? where id =?",
+               person.getName(),person.getAge(),person.getEmail(),id);
+    }
+
+    public void delete(int id) {
+       jdbcTemplate.update("Delete from person where id=?",id);
+    }
+}
